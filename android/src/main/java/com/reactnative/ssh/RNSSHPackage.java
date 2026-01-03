38510f7ef10b5d4a06d769c9a0fffb53d89a4bbe
Abdulraheem A Omeiza <blesseth.omeiza@gmail.com>
2026-01-03 15:59:30 +0100

    Remove deprecated createJSModules method from RNSSHPackage

--- android/src/main/java/com/reactnative/ssh/RNSSHPackage.java
+++ android/src/main/java/com/reactnative/ssh/RNSSHPackage.java
@@ -14,11 +14,6 @@ import com.facebook.react.uimanager.ViewManager;
 public class RNSSHPackage implements ReactPackage {
 
   @Override
-  public List<Class<? extends JavaScriptModule>> createJSModules() {
-    return Collections.emptyList();
-  }
-
-  @Override
   public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
     return Collections.emptyList();
   }
